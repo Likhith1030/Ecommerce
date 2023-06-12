9b7ce5970210d782c97cd767c82fe4304a22f707
Likhith1030 <95261477+Likhith1030@users.noreply.github.com>
2023-06-12 10:12:27 +0530

    blogs added

--- garage/src/App.js
+++ garage/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
+import Articles from "./components/Articles";
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,7 @@ function App() {
           <Route path="/signin" element={<SignIn />} />
           <Route path="/" element={<HomePage />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/blog" element={<Articles />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ garage/src/components/Articles.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { Pagination, Paper } from "@mui/material";
+import articleData from "../imgs/blogs.json";
+import Navbar from "../components/Navbar";
+import "../App.css";
+
+const Articles = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const articlesPerPage = 10;
+
+  // Calculate the index range for the current page
+  const indexOfLastArticle = currentPage * articlesPerPage;
+  const indexOfFirstArticle = indexOfLastArticle - articlesPerPage;
+  const currentArticles = Object.entries(articleData).slice(
+    indexOfFirstArticle,
+    indexOfLastArticle
+  );
+
+  // Handle page change
+  const handlePageChange = (event, pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="blogs">
+        {currentArticles.map(([title, article], index) => (
+          <Paper
+            key={index}
+            elevation={3}
+            sx={{
+              padding: "1rem",
+              margin: "1rem",
+              marginLeft: "auto",
+              marginRight: "auto",
+              maxWidth: "600px",
+            }}
+          >
+            <h2>{title}</h2>
+            <p>{article[0][0]}</p>
+            <a href={article[0][1]}>Read More</a>
+          </Paper>
+        ))}
+
+        {/* Pagination */}
+        <div>
+          {Object.entries(articleData).length > articlesPerPage && (
+            <Pagination
+              count={Math.ceil(
+                Object.entries(articleData).length / articlesPerPage
+              )}
+              page={currentPage}
+              onChange={handlePageChange}
+              sx={{
+                padding: "1rem",
+                margin: "1rem",
+                marginLeft: "auto",
+                marginRight: "auto",
+                maxWidth: "350px",
+              }}
+            />
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Articles;
