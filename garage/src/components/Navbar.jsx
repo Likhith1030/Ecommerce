952a7f817f0574a7e338313dda16bbd4ed22fc97
Likhith1030 <95261477+Likhith1030@users.noreply.github.com>
2023-06-14 14:36:23 +0530

    asd

--- garage/src/components/Navbar.jsx
+++ garage/src/components/Navbar.jsx
@@ -100,7 +100,12 @@ function ResponsiveAppBar() {
               }}
             >
               {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                <MenuItem
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  component={Link}
+                  to={`/${page.toLowerCase()}`}
+                >
                   <Typography textAlign="center">{page}</Typography>
                 </MenuItem>
               ))}
